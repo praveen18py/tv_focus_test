7102a170bb865d35ee6174b6dba5d5dabf7ecc80
Praveen Kumar Pandey <69620431+praveenkprobosoftin@users.noreply.github.com>
2020-09-15 12:48:54 +0530

    cleanup

--- app/src/main/java/com/example/sampletvfocustest/recycler/RecyclerApproachActivity.kt
+++ app/src/main/java/com/example/sampletvfocustest/recycler/RecyclerApproachActivity.kt
@@ -8,42 +8,34 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.sampletvfocustest.R
 import com.example.sampletvfocustest.extras.SmoothScroller
 import com.example.sampletvfocustest.utils.convertDpToPixels
-import kotlinx.android.synthetic.main.activity_awesome.*
+import kotlinx.android.synthetic.main.activity_recycler_approach.*
 import java.util.*
 
 class RecyclerApproachActivity : Activity() {
 
     private var previouslyFocusedIndex: Int = -1
     private lateinit var collapsedToolbarScrollListener: RecyclerView.OnScrollListener
-
     private var currentFocusedView: View? = null
 
-    private fun performSmoothScrollToPosition(
-        recyclerView: RecyclerView,
-        scroller: SmoothScroller,
-        focusedPosition: Int?
-    ) {
-        val layoutManager = recyclerView.layoutManager as StaggeredGridLayoutManager
-        focusedPosition?.let { position ->
-            scroller.startSmoothScroll(layoutManager, position)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_awesome)
-        checkFocus()
-        initAdapter()
+        setContentView(R.layout.activity_recycler_approach)
         initViews()
     }
 
     private fun initViews() {
+        initMenuItems()
+        initAdapter()
+        checkFocus()
+    }
+
+    private fun initMenuItems() {
         for (i in 1..5) {
             val imageView = ImageView(this)
             imageView.setImageResource(R.drawable.menu_image_selector)
@@ -53,54 +45,23 @@ class RecyclerApproachActivity : Activity() {
                 convertDpToPixels(50f, this).toInt(),
                 convertDpToPixels(50f, this).toInt()
             )
-
-            imageView.setOnFocusChangeListener { _, _ ->
-                val find = llMenu.children.find { it.isFocused }
-                if (find == null) {
-                    Log.e("previouslyFocusedIndex", "out focus $previouslyFocusedIndex")
-//                    pageRecycler.scrollToPosition(previouslyFocusedIndex)
-//                    pageRecycler.scrollTo(0, previouslyFocusedIndex)
-//                     pageRecycler.scrollToPosition(previouslyFocusedIndex)
-
-//                    performSmoothScrollToPosition(
-//                        pageRecycler,
-//                        SmoothScroller(this@AwesomeActivity),
-//                        previouslyFocusedIndex
-//                    )
-
-                    // pageRecycler.focusableViewAvailable(currentFocusedView)
-                } else {
-                    Log.e("previouslyFocusedIndex", "In focus $previouslyFocusedIndex")
-
-                }
-            }
-
             llMenu.addView(imageView, linLayPar)
         }
     }
 
     private fun initAdapter() {
         pageRecycler.layoutManager =
-            StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
-        pageRecycler.adapter =
-            PageRecyclerAdapter()
+            StaggeredGridLayoutManager(
+                1, StaggeredGridLayoutManager.VERTICAL
+            )
+        pageRecycler.adapter = PageRecyclerAdapter()
         setOnScrollListener(true)
-
-        // pageRecycler.preserveFocusAfterLayout = true
-
-        pageRecycler.setOnFocusChangeListener { view, b ->
-            println("view is $view, and it is $b")
-//            currentFocusedView = view
-        }
-        pageRecycler.preserveFocusAfterLayout = true
     }
 
     private fun setOnScrollListener(isAddListener: Boolean) {
         if (isAddListener) {
             collapsedToolbarScrollListener = object : RecyclerView.OnScrollListener() {
-                val scroller =
-                    SmoothScroller(this@RecyclerApproachActivity)
-
+                val scroller = SmoothScroller(this@RecyclerApproachActivity)
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                     super.onScrolled(recyclerView, dx, dy)
                     scrollToPosition(recyclerView, scroller)
@@ -127,6 +88,16 @@ class RecyclerApproachActivity : Activity() {
         }
     }
 
+    private fun performSmoothScrollToPosition(
+        recyclerView: RecyclerView,
+        scroller: SmoothScroller,
+        focusedPosition: Int?
+    ) {
+        val layoutManager = recyclerView.layoutManager as StaggeredGridLayoutManager
+        focusedPosition?.let { position ->
+            scroller.startSmoothScroll(layoutManager, position)
+        }
+    }
 
     private fun checkFocus() {
         val timerObj = Timer()
